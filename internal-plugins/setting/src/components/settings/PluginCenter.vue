03548194045d215c199c1fec3c37c0de5f3cd8f6
cjhgit <1418503647@qq.com>
2026-02-20 02:26:52 +0800

    feat：插件打包功能从列表移除，保持列表简洁

--- internal-plugins/setting/src/components/settings/PluginCenter.vue
+++ internal-plugins/setting/src/components/settings/PluginCenter.vue
@@ -108,30 +108,6 @@
                 </svg>
               </button>
               <button
-                v-if="plugin.isDevelopment"
-                class="icon-btn package-btn"
-                :disabled="isPackaging"
-                title="打包插件为 ZIP"
-                @click.stop="handlePackagePlugin(plugin)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polyline points="16 16 12 12 8 16"></polyline>
-                  <line x1="12" y1="12" x2="12" y2="21"></line>
-                  <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                  <polyline points="16 16 12 12 8 16"></polyline>
-                </svg>
-              </button>
-              <button
                 class="icon-btn reload-btn"
                 :disabled="isReloading"
                 title="重新加载 plugin.json 配置文件"
@@ -815,14 +791,6 @@ function closePluginDetail(): void {
 
 .folder-btn:hover {
   background: var(--primary-light-bg);
-}
-
-.package-btn {
-  color: var(--purple-color);
-}
-
-.package-btn:hover:not(:disabled) {
-  background: var(--purple-light-bg);
 }
 
 .reload-btn {
